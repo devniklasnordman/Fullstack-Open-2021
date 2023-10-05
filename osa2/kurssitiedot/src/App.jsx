c2b7e663246864353fbd0365fa8153c6ad0bb940
niklas.nordman <niklas.nordman@edu.turkuamk.fi>
2023-10-06 01:11:18 +0300

    2.5: erillinen moduuli

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,40 +1,4 @@
-const Header = ({ name }) => {
-  return <h2>{name}</h2>
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <b>Number of exercises {total}</b>
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-        {parts.map(part =>
-        <Part key={part.id} part={part}/>
-        )}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
