70ae5931f613de0559becb1a5a443da4e539efaa
niklas.nordman <niklas.nordman@edu.turkuamk.fi>
2023-12-08 01:22:02 +0200

    5.14: blogilistan testit, step2

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -27,5 +27,29 @@ test('renders only title, but no author, url or likes', () => {
   expect(authorElement).toBeNull()
   expect(urlElement).toBeNull()
   expect(likesElement).toBeNull()
+})
 
+test('renders all blog details after button click', async () => {
+  const blog = {
+    title: 'Testi blogi',
+    author: 'Testi Timppa',
+    url: 'www.timppatestaa.fi',
+    likes: 20,
+    user: { name: 'Testi Tiina'}
+  }
+
+  render(
+    <Blog blog={blog} setBlogs={() => {}} user={{ username: 'Testi Tiina' }} />)
+  
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const urlElement = screen.getByText('www.timppatestaa.fi')
+  const likesElement = screen.getByText('likes 20')
+  const userElement = screen.getByText('added by Testi Tiina')
+
+  expect(urlElement).toBeDefined()
+  expect(likesElement).toBeDefined()
+  expect(userElement).toBeDefined()
 })
